db37c3afba826b6ff6c19a0f89ff05f8a29e4491
hafidh <hafidh.difa@gmail.com>
2024-12-17 19:59:17 +0700

    initial commit for laravel project

--- database/seeders/Settings.php
+++ database/seeders/Settings.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Setting;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class Settings extends Seeder
 {
@@ -12,6 +13,24 @@ class Settings extends Seeder
      */
     public function run(): void
     {
-        //
+        $setting = [
+            [
+                'category_id' => '2',
+                'supplier_id' => '2',
+                'name' => 'Meja',
+                'sku' => 'MJ123',
+                'stock' => '0',
+                'stockMinimum' => '5',
+                'description' => 'Dari kayu.',
+                'purchase_price' => '50000',
+                'selling_price' => '60000',
+                'image' => 'images/product/meja.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+            ];
+        foreach ($setting as $settingData) {
+            Setting::create($settingData);
+        }
     }
 }

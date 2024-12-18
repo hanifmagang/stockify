dc926facd4a06f000a65cfbd1fc16f949a7439c6
hafidh <hafidh.difa@gmail.com>
2024-12-18 15:05:53 +0700

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategoryAwal::class);
         $this->call(SupplierAwal::class);
         $this->call(ProductAwal::class);
+        $this->call(TransactionAwal::class);
     }
 }

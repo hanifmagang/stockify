78fd10a31a8b89fb61111d2a2154bea376d27a50
hafidh <hafidh.difa@gmail.com>
2024-12-22 15:10:19 +0700

    ubah migration

--- database/migrations/2024_12_20_133736_add_foto_to_users_table.php
+++ database/migrations/2024_12_20_133736_add_foto_to_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
 
 return new class extends Migration
 {
@@ -11,6 +12,19 @@ return new class extends Migration
      */
     public function up(): void
     {
+        $defaultPath = 'public/images/profile';
+        $defaultFile = $defaultPath . '/ppdepresi.png';
+
+        if (!Storage::exists($defaultPath)) {
+            Storage::makeDirectory($defaultPath);
+        }
+
+        // Pastikan file default tersedia
+        if (!Storage::exists($defaultFile)) {
+            Storage::put($defaultFile, file_get_contents(public_path('images/profile/ppdepresi.png')));
+        }
+
+        // Update struktur tabel users
         Schema::table('users', function (Blueprint $table) {
             $table->string('foto')->default('ppdepresi.png');
         });
@@ -22,7 +36,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('foto')->default(null)->change();
         });
     }
 };
